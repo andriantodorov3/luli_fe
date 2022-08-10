5cd86540a22436c8ebee2232ec3a3f30a20e5d37
Andrian Todorov <andrian.todorov@gmail.com>
2022-08-10 12:34:42 +0300

    backend api requests with axios, cleanup of default template components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,111 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="new-capsule-wrapper lighten-5" :fluid="true">
+    <v-row justify="space-around">
+      <v-col col="6" sm="12" md="8">
+        <v-card class="search-page elevation-5">
+          <v-card-title>Stocks analyst</v-card-title>
+          <v-card-text>
+            <p>
+              Use the form below to see how much money you could have made by
+              buying and selling stocks X.<br />
+              You need to provide the amount of money you are willing to invest
+              and the timeframe you are interested in.<br />
+              Provided results will show you the best possible time for buying
+              (in the past) and selling and will calculate the profit you could
+              have made.
+            </p>
+            <stock-search-form
+              :loading="searching"
+              @search="search"
+              :start_errors="start_errors"
+              :end_errors="end_errors"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col col="6" sm="12" md="4">
+        <v-card class="search-page-results elevation-5">
+          <v-card-title>Results</v-card-title>
+          <v-card-text>
+            <p>Use the form on the left side to define your criteria</p>
+
+            <search-results
+              v-if="searchSuccess"
+              v-bind="searchResults"
+              :funds="searchPayload.funds"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import StockSearchForm from "../components/StockSearchForm.vue";
 
+import { mapGetters } from "vuex";
+import { searchStocks } from "@/_api/";
+import SearchResults from "./SearchResults.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    StockSearchForm,
+    SearchResults,
+  },
+
+  computed: {
+    ...mapGetters(["bus"]),
+  },
+  data() {
+    return {
+      searching: false,
+      searchResults: null,
+      searchSuccess: false,
+      searchPayload: null,
+      start_errors: [],
+      end_errors: [],
+    };
+  },
+
+  methods: {
+    async search(payload) {
+      this.searching = true;
+      this.searchPayload = payload;
+
+      try {
+        const apiData = await searchStocks(payload);
+        this.searchResults = apiData.data;
+        this.searchSuccess = true;
+
+        this.start_errors = [];
+        this.end_errors = [];
+
+        //handle errors
+        this.searching = false;
+      } catch (error) {
+        this.searchSuccess = false;
+        if (
+          error?.response?.data?.statusCode === 400 &&
+          error?.response?.data?.success === false
+        ) {
+          const backend_errors = error.response.data.error.errors;
+          if (backend_errors.start_time != undefined) {
+            this.start_errors = backend_errors.start_time;
+          }
+          if (backend_errors.end_time != undefined) {
+            this.end_errors = backend_errors.end_time;
+          }
+          //backend API returns payload with errors
+        } else {
+          this.bus.$emit("shownotification", {
+            msg: "Cannot reach backend, error: " + error,
+            type: "error",
+          });
+        }
+        this.searching = false;
+      }
+    },
+  },
+};
 </script>
