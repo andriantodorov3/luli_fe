e0d73bf95af9ef6e7022dbc08d4d6b126d121951
Andrian Todorov <andrian.todorov@gmail.com>
2022-08-10 12:05:49 +0300

    vuex

---
+++ src/store/vuex.state.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+export const state = {
+    //event bus
+    bus: new Vue(),
+    drawer: null,
+    //couldnt find a good looking picture, keeping these in the template anyways, if you have any idea, let me know
+    drawerImage: true,
+    image: null,
+    mini: false,
+    gradient: '',
+
+}
