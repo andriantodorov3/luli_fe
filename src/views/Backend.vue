6ec18a3b93dd7f1faf507715eac09ab0b4f19a40
Andrian Todorov <andrian.todorov@gmail.com>
2022-08-14 01:24:24 +0300

    backend component + tests

--- src/views/Backend.vue
+++ src/views/Backend.vue
@@ -1,14 +1,27 @@
 <template>
-  <p>backend</p>
+  <div>
+    <faq-renderer :items="items" />
+  </div>
 </template>
 
 <script>
+import FaqRenderer from "../components/FaqRenderer.vue";
 export default {
-    name: 'backend'
-
-}
+  name: "backend",
+  components: {
+    FaqRenderer,
+  },
+  data() {
+    FaqRenderer;
+    return {
+      items: [
+        {
+          subject: "backend is built with vue",
+          title: "Why Vue",
+          text: "Because...",
+        },
+      ],
+    };
+  },
+};
 </script>
-
-<style>
-
-</style>
---
+++ src/views/__tests__/Backend.spec.js
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils'
+import Komponent from '@/views/Backend.vue'
+import FaqRenderer from "@/components/FaqRenderer.vue";
+
+describe('Backend.vue', () => {
+    it('Renders faqs', () => {
+        const wrapper = mount(Komponent)
+        expect(wrapper.findAllComponents(FaqRenderer).length).toBe(wrapper.vm.items.length)
+    })
+
+})
