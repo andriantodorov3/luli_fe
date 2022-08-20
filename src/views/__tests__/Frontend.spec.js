69f1cc9dc761d964e761d30046f7e58c4a0f2a1b
Andrian Todorov <andrian.todorov@gmail.com>
2022-08-20 13:50:58 +0300

    fix tests to check for a single element

--- src/views/__tests__/Frontend.spec.js
+++ src/views/__tests__/Frontend.spec.js
@@ -5,7 +5,7 @@ import FaqRenderer from "@/components/FaqRenderer.vue";
 describe('Frontend.vue', () => {
     it('Renders faqs', () => {
         const wrapper = mount(Komponent)
-        expect(wrapper.findAllComponents(FaqRenderer).length).toBe(wrapper.vm.items.length)
+        expect(wrapper.findAllComponents(FaqRenderer).length).toBe(1)
     })
 
 })
